adb83c7fd316424e1b4801f9b627ea6845856863
Josh Zuech <josh.zuech@nextthought.com>
2021-09-01 16:18:53 -0500

    Single ScormCloudService impl

--- src/nti/scorm_cloud/client/__init__.py
+++ src/nti/scorm_cloud/client/__init__.py
@@ -16,11 +16,8 @@ try:
     from urllib2 import urlopen
 except ImportError:  # pragma: no cover
     from urllib.request import urlopen
+    
 from zope import interface
-
-from rustici_software_cloud_v2.configuration import Configuration as SCV2Configuration
-from rustici_software_cloud_v2.api_client import ApiClient as SCV2ApiClient
-
 
 from nti.scorm_cloud.client.config import Configuration
 
@@ -40,6 +37,8 @@ from nti.scorm_cloud.client.request import ServiceRequest
 from nti.scorm_cloud.client.request import ScormCloudError
 from nti.scorm_cloud.client.request import ScormCloudUtilities
 
+from nti.scorm_cloud.client.scorm import ScormCloudService
+
 from nti.scorm_cloud.client.tag import TagService
 
 from nti.scorm_cloud.interfaces import ITagSettings
@@ -49,90 +48,10 @@ from nti.scorm_cloud.interfaces import IReportingService
 from nti.scorm_cloud.interfaces import IDateRangeSettings
 from nti.scorm_cloud.interfaces import IScormCloudService
 from nti.scorm_cloud.interfaces import IRegistrationService
-from nti.scorm_cloud.interfaces import IDateRangeSettings
 
 
 logger = __import__('logging').getLogger(__name__)
 
-
-@interface.implementer(IScormCloudService)
-class ScormCloudService(object):
-
-    def __init__(self, configuration):
-        self.config = configuration
-        self.v2config = SCV2Configuration()
-        self.v2config.username = self.config.appid
-        self.v2config.password = self.config.secret
-        self.__handler_cache = {}
-
-    @classmethod
-    def withconfig(cls, config):
-        """
-        Named constructor that creates a ScormCloudService with the specified
-        Configuration object.
-
-        Arguments:
-        config -- the Configuration object holding the required configuration
-            values for the SCORM Cloud API
-        """
-        return cls(config)
-
-    @classmethod
-    def withargs(cls, appid, secret, serviceurl,
-                 origin='rusticisoftware.pythonlibrary.2.0.0'):
-        """
-        Named constructor that creates a ScormCloudService with the specified
-        configuration values.
-
-        Arguments:
-        appid -- the AppID for the application defined in the SCORM Cloud
-            account
-        secret -- the secret key for the application
-        serviceurl -- the service URL for the SCORM Cloud web service. For
-            example, http://cloud.scorm.com/EngineWebServices
-        origin -- the origin string for the application software using the
-            API/Python client library
-        """
-        return cls(Configuration(appid, secret, serviceurl, origin))
-
-    def make_v2_api(self):
-        # TODO should this be Lazy, or CachedProperty?
-        # it wraps a ConnectionPool which could be useful to share
-        return SCV2ApiClient(configuration=self.v2config)
-
-    def get_tag_service(self):
-        return TagService(self)
-
-    def get_course_service(self):
-        return CourseService(self)
-
-    def get_debug_service(self):
-        return DebugService(self)
-
-    def get_registration_service(self):
-        return RegistrationService(self)
-
-    def get_invitation_service(self):
-        return InvitationService(self)
-
-    def get_reporting_service(self):
-        return ReportingService(self)
-
-    def get_upload_service(self):
-        return UploadService(self)
-
-    def request(self):
-        """
-        Convenience method to create a new ServiceRequest.
-        """
-        return ServiceRequest(self)
-
-    def make_call(self, method):
-        """
-        Convenience method to create and call a simple ServiceRequest (no
-        parameters).
-        """
-        return self.request().call_service(method)
 
 @interface.implementer(IDateRangeSettings)
 class DateRangeSettings(object):
